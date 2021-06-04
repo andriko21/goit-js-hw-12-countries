35bb25403b2cc5986385dba6660002855e7d40d5
andriko21 <75207634+andriko21@users.noreply.github.com>
2021-06-04 16:58:00 +0300

    зробив все

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -5,7 +5,7 @@ const refs = getRefs();
 export default function fetchCountries(searchQuery) {
   const baseUrl = 'https://restcountries.eu/rest/v2/name/';
   return fetch(baseUrl + searchQuery)
-    .then(response => response.json()).then(el => { console.log(el); return el })
-    .catch(console.log('error'))
+    .then(response => response.json())
+    .catch(alert({ text: 'Nothing matches the search'}))
 };
 

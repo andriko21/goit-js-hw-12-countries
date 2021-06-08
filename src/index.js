43fa9a3c2cc0617043c2766de3d1b1d0cd652e69
andriko21 <75207634+andriko21@users.noreply.github.com>
2021-06-08 11:04:27 +0300

    виправив помилку

--- src/index.js
+++ src/index.js
@@ -18,14 +18,15 @@ refs.input.addEventListener('input', _debounce(indentifyValue, 700));
 function indentifyValue(ev) {
   const textOfInput = ev.target.value.trim();
   if (textOfInput.length < 1) {
+    clearDom()
     return alert({ text: 'Nothing matches the search'});
   }
   return fetchCountries(textOfInput).then(data =>
     data.length <= 10 && data.length >= 2
       ? createMaktup(data)
       : data.length === 1
-      ? createCard(data)
-      : alert({ text: 'Too many matches found. Please enter a more specific query'}),
+      ? createCard(data) 
+      : alert({ text: 'Too many matches found. Please enter a more specific query' }),
   );
 }
 
@@ -41,38 +42,7 @@ function createCard(mak) {
 function clearInput() {
   refs.input.value = '';
 }
-// const _debounce = require('lodash.debounce');
-// import fetchCountries from './js/fetchCountries.js';
-// import card from './templates/card.hbs'
-// import maktup from './templates/maktup.hbs'
-// import './sass/main.scss';
-// import getRefs from './js/refs.js';
-// const refs = getRefs();
-// refs.input.addEventListener('input', _debounce(indentifyValue, 700));
 
-// function indentifyValue(ev) {
-//   const textOfInput = ev.target.value.trim();
-//   if (textOfInput.length < 1) {
-//     return console.log('Введи щось нормальне');
-//   }
-//   return fetchCountries(textOfInput).then(data =>
-//     data.length <= 10 && data.length >= 2
-//       ? createMaktup(data)
-//       : data.length === 2
-//       ? createCard(data)
-//       : console.log('error'),
-//   );
-// }
-
-// function createMaktup(mak) {
-//   refs.section.innerHTML = maktup(mak);
-// }
-
-// function createCard(mak) {
-//   refs.section.innerHTML = card(mak);
-//   clearInput();
-// }
-
-// function clearInput() {
-//   refs.input.value = '';
-// }
+function clearDom() {
+  refs.section.innerHTML = '';
+}

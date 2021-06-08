b1bfaeaab020592e9d49b23b8a91dc3cb98c54f2
andriko21 <75207634+andriko21@users.noreply.github.com>
2021-06-08 11:40:39 +0300

    Виправив помилку

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const _debounce = require('lodash.debounce');
+import debounce from 'lodash/debounce';
 import fetchCountries from './js/fetchCountries.js';
 import { alert, defaultModules } from '@pnotify/core';
 import * as PNotifyMobile from '@pnotify/mobile';
@@ -13,20 +13,20 @@ import './sass/main.scss';
 defaultModules.set(PNotifyMobile, {});
 
 const refs = getRefs();
-refs.input.addEventListener('input', _debounce(indentifyValue, 700));
+refs.input.addEventListener('input', debounce(indentifyValue, 700));
 
 function indentifyValue(ev) {
   const textOfInput = ev.target.value.trim();
   if (textOfInput.length < 1) {
     clearDom()
-    return alert({ text: 'Nothing matches the search'});
+    return alert({ text: 'Nothing matches the search, index' });
   }
   return fetchCountries(textOfInput).then(data =>
     data.length <= 10 && data.length >= 2
       ? createMaktup(data)
       : data.length === 1
-      ? createCard(data) 
-      : alert({ text: 'Too many matches found. Please enter a more specific query' }),
+        ? createCard(data)
+        : alert({ text: 'Too many matches found. Please enter a more specific query' }),
   );
 }
 
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,3 +1,6 @@
+import { alert, defaultModules } from '@pnotify/core';
+import '@pnotify/mobile/dist/PNotifyMobile.css';
+import '@pnotify/core/dist/PNotify.css';
 import getRefs from './refs.js';
 
 const refs = getRefs();
@@ -6,6 +9,5 @@ export default function fetchCountries(searchQuery) {
   const baseUrl = 'https://restcountries.eu/rest/v2/name/';
   return fetch(baseUrl + searchQuery)
     .then(response => response.json())
-    .catch(alert({ text: 'Nothing matches the search'}))
-};
-
+    .catch(alert({ text: 'Nothing matches the search fetchCountries' }));
+}
